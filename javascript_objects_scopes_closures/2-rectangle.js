db5e5f7cf9a0141c7d1dfc5a41b9219488e2d8ca
Linssssssss <99273313+Linssssssss@users.noreply.github.com>
2023-09-07 12:10:25 +0300

    Rectangle

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
 class Rectangle {
   constructor(w, h) {
-    if (w <= 0 || h <= 0 || typeof w !== "number" || typeof h !== "number") {
-      throw new Error("Width and height must be positive integers");
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
-
-    this.width = w;
-    this.height = h;
   }
 }
 
