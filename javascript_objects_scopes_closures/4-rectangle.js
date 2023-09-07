a9218c25d14bf97b87af23943677ccf8d8b0de19
Linssssssss <99273313+Linssssssss@users.noreply.github.com>
2023-09-07 11:56:05 +0300

    Rectangle #4

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -1,7 +1,8 @@
 class Rectangle {
   constructor(w, h) {
     if (w <= 0 || h <= 0) {
-      return {}; // Return an empty object if w or h is not a positive integer
+      // Create an empty object if w or h is not a positive integer
+      return {};
     }
 
     this.width = w;
@@ -12,6 +13,17 @@ class Rectangle {
     for (let i = 0; i < this.height; i++) {
       console.log("X".repeat(this.width));
     }
+  }
+
+  rotate() {
+    // Swap the values of width and height
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double() {
+    // Double the width and height of the rectangle
+    this.width *= 2;
+    this.height *= 2;
   }
 }
 
