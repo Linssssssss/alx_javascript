6949fbd8b52574d7850bb8384ee1f7137d4d5493
Linssssssss <99273313+Linssssssss@users.noreply.github.com>
2023-09-13 19:41:33 +0300

    Who was playing in this movie

--- javascript-network/5-starwars_characters.js
+++ javascript-network/5-starwars_characters.js
@@ -1,20 +1,19 @@
 const request = require("request");
 
-// Get the movie ID from the command line argument
-const movieID = process.argv[2];
+const movieId = process.argv[2];
 
-// Construct the URL for the Star Wars API endpoint
-const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieID}`;
+const url = `https://swapi.dev/api/films/${movieId}/`;
 
-// Make a GET request to the API endpoint
-request.get(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error(`Error: ${error.message}`);
-  } else {
-    // Parse the JSON response
-    const movieData = JSON.parse(body);
+request(url, (err, response, body) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
 
-    // Print the character names for the movie
-    console.log(movieData.characters.join("\n"));
+  const film = JSON.parse(body);
+  const characters = film.characters;
+
+  for (const character of characters) {
+    console.log(character);
   }
 });
