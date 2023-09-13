d793f1d7ab17994a4146e5b32980cccbb3d67616
Linssssssss <99273313+Linssssssss@users.noreply.github.com>
2023-09-13 19:48:50 +0300

    Loripsum

--- javascript-network/3-request_store.js
+++ javascript-network/3-request_store.js
@@ -1,17 +1,19 @@
-const request = require("request");
+#!/usr/bin/node
 const fs = require("fs");
+const request = require("request");
 
-// Get the URL and file path from the command line arguments
 const url = process.argv[2];
 const filePath = process.argv[3];
 
-// Make a GET request to the specified URL
-request.get(url, (error, response, body) => {
-  if (error) {
-    console.error(`Error: ${error.message}`);
-  } else {
-    // Write the response body to the specified file with UTF-8 encoding
-    fs.writeFileSync(filePath, body, "utf-8");
-    console.log(`Contents of the webpage have been saved to ${filePath}`);
+request(url, (err, response, body) => {
+  if (err) {
+    console.log(err);
+    process.exit(1);
   }
+  fs.writeFile(filePath, body, "utf-8", (err, data) => {
+    if (err) {
+      console.log(err);
+      process.exit(1);
+    }
+  });
 });
