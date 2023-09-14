8481536ef8fe3a455ff148115b6a7c461f3b4290
Linssssssss <99273313+Linssssssss@users.noreply.github.com>
2023-09-14 16:36:22 +0300

    Lexical scoping and welcome

---
+++ Javascript_advanced/0-welcome.js
@@ -0,0 +1,12 @@
+function welcome(firstName, lastName) {
+  var fullName = firstName + " " + lastName;
+
+  function displayFullName() {
+    alert("Welcome " + fullName + "!");
+  }
+
+  displayFullName();
+}
+
+// Testing the function
+welcome("Holberton", "School");
