4b007e3a32aa173ca3df1598d070f3262512e6fc
Linssssssss <99273313+Linssssssss@users.noreply.github.com>
2023-09-19 09:16:20 +0300

    Delete cookie and mini application

--- Cookies_local_storage/src/index.js
+++ Cookies_local_storage/src/index.js
@@ -37,3 +37,51 @@ function showCookies() {
   p.innerHTML = `Email: ${emailCookie} - Firstname: ${firstnameCookie}`;
   cookieOutput.appendChild(p);
 }
+function showForm() {
+  const welcomeMessage = document.getElementById("welcomeMessage");
+  if (welcomeMessage) {
+    welcomeMessage.remove();
+  }
+  const formContainer = document.getElementById("formContainer");
+  formContainer.style.display = "block";
+}
+
+function hideForm() {
+  const formContainer = document.getElementById("formContainer");
+  formContainer.style.display = "none";
+}
+
+function deleteCookiesAndShowForm() {
+  document.cookie =
+    "firstname=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+  document.cookie = "email=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+  showForm();
+}
+
+function showWelcomeMessageOrForm() {
+  const emailCookie = getCookie("email");
+  const firstnameCookie = getCookie("firstname");
+
+  const welcomeContainer = document.getElementById("welcomeContainer");
+  const formContainer = document.getElementById("formContainer");
+
+  if (!emailCookie || !firstnameCookie) {
+    showForm();
+  } else {
+    // Clear existing content
+    welcomeContainer.innerHTML = "";
+
+    const welcomeMessage = document.createElement("h1");
+    welcomeMessage.textContent = `Welcome ${firstnameCookie} `;
+    const logoutLink = document.createElement("a");
+    logoutLink.textContent = "(logout)";
+    logoutLink.style.fontWeight = "normal";
+    logoutLink.style.fontStyle = "italic";
+    logoutLink.style.marginLeft = "10px";
+    logoutLink.href = "javascript:void(0)";
+    logoutLink.addEventListener("click", deleteCookiesAndShowForm);
+
+    welcomeMessage.appendChild(logoutLink);
+    welcomeContainer.appendChild(welcomeMessage);
+  }
+}
