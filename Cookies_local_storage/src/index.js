82262a64f7ca753bda1ca06ac5ab870e24689d9f
Linssssssss <99273313+Linssssssss@users.noreply.github.com>
2023-09-19 09:12:11 +0300

    Read Cookie

--- Cookies_local_storage/src/index.js
+++ Cookies_local_storage/src/index.js
@@ -3,36 +3,37 @@ function setCookies() {
   const emailInput = document.getElementById("email");
 
   if (firstnameInput.value && emailInput.value) {
-    document.cookie = `firstname=${firstnameInput.value}; expires=Fri, 31 Dec 9999 23:59:59 GMT`;
-    document.cookie = `email=${emailInput.value}; expires=Fri, 31 Dec 9999 23:59:59 GMT`;
+    const expirationDate = new Date();
+    expirationDate.setDate(expirationDate.getDate() + 10); // Expires in 10 days
+
+    document.cookie = `firstname=${
+      firstnameInput.value
+    }; expires=${expirationDate.toUTCString()}`;
+    document.cookie = `email=${
+      emailInput.value
+    }; expires=${expirationDate.toUTCString()}`;
   }
 }
 
+function getCookie(name) {
+  const cookies = document.cookie.split("; ");
+  for (const cookie of cookies) {
+    const [cookieName, cookieValue] = cookie.split("=");
+    if (cookieName === name) {
+      return decodeURIComponent(cookieValue);
+    }
+  }
+  return "";
+}
+
 function showCookies() {
+  const emailCookie = getCookie("email");
+  const firstnameCookie = getCookie("firstname");
+
   const cookieOutput = document.getElementById("cookieOutput");
   cookieOutput.innerHTML = "";
 
-  const cookies = document.cookie.split("; ");
-  for (const cookie of cookies) {
-    const [name, value] = cookie.split("=");
-    const p = document.createElement("p");
-    p.innerHTML = `Cookies: ${name}=${value}`;
-    cookieOutput.appendChild(p);
-  }
-  function setCookies() {
-    const firstnameInput = document.getElementById("firstname");
-    const emailInput = document.getElementById("email");
-
-    if (firstnameInput.value && emailInput.value) {
-      const expirationDate = new Date();
-      expirationDate.setDate(expirationDate.getDate() + 10); // Expires in 10 days
-
-      document.cookie = `firstname=${
-        firstnameInput.value
-      }; expires=${expirationDate.toUTCString()}`;
-      document.cookie = `email=${
-        emailInput.value
-      }; expires=${expirationDate.toUTCString()}`;
-    }
-  }
+  const p = document.createElement("p");
+  p.innerHTML = `Email: ${emailCookie} - Firstname: ${firstnameCookie}`;
+  cookieOutput.appendChild(p);
 }
